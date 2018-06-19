2a6c91b434b557824e82639b68e4dc705b151831
Gavin Trevino <gavintrevino@gmail.com>
2018-06-18 19:38:10 -0500

    Initial commit of php site for ECT 436

---
+++ signup.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Join the Fun!</title>
+  <link rel="stylesheet" type="text/css" href="signup.css">
+</head>
+<body>
+
+<div>
+  <h1>Sign Up!</h1>
+  <form action="welcome.php" method="GET">
+  First Name: <input type="text" name="FirstName" required>
+  <br><br>
+  Last Name: <input type="text" name="LastName" required>
+  <br><br>
+  E-mail: <input type="text" name="Email" required>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">
+</form>
+  </body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+  <title>Join the Fun!</title>
+  <link rel="stylesheet" type="text/css" href="signup.css">
+</head>
+<body>
+
+<div>
+  <table>
+    <tr>
+      <th>Welcome <?php echo $_GET["FirstName"]; ?>, Thank you for joing our team!</th>
+    </tr>
+    <tr>
+      <th><button type="button" onclick="window.location='http://ectweb.cs.depaul.edu/GTREVIN1/Database/index.php'">Home</button></th>
+    </tr>
+
+</table>
+</div>
+
+</body>
+</html>
